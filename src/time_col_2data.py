b46373a5e87607bce63ef57064208a1e719f2d35
Clément Adandé <132694770+clemsadand@users.noreply.github.com>
2024-06-13 15:21:17 +0200

    Update time_col_2data.py

--- src/time_col_2data.py
+++ src/time_col_2data.py
@@ -1,9 +1,9 @@
-#import os
-
+# Open a file data containing a dataset without head
 with open("data.csv", "r") as f:
 	X = f.readlines()
 
-with open("dataset.csv", "w") as g:
+# Add a new column with for time in the format DD/MM/YYYY
+with open("data.csv", "w") as g:
 	for line in X[:-2]:
 		l = line.split("\n")[0].split(",")
 		t = l[0] + "-"+l[1] + "-"+ l[2]
